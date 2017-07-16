607243eefca1d1a822a8df6b60726e3ddcbb2600
moon7392 <moon7392@naver.com>
2017-07-16 21:21:21 +0900

    realfile3

--- pythontest.py
+++ pythontest.py
@@ -2,34 +2,30 @@ import pythonwhois
 import sys
 import json
 
-
-arguments=sys.argv[1]#we can get filename through argv
-#file_data=OrderedDict()
-
-f=open(arguments,'r')
-
-data=f.readline()
-
-webdata=pythonwhois.get_whois(data)#we can get whois data
-
-f2=open("jsonfiles",'w')
-#print webdata['nameservers']
-
-
-def date_handler(obj):		#this funciton in order to catch timeerror
+#This function can catch typeeroor
+def date_handler(obj):		
     if hasattr(obj, 'isoformat'):
         return obj.isoformat()
     else:
         raise TypeError
-#// use two argument, we can catch timeerror,this is json encoding
-data2=json.dumps(webdata,default=date_handler)
 
-f2.write(data2)
+# we have to write filename
+argumens=sys.argv[1]
+f=open(argumens,'r')
 
-#file_data["status"]=data[status}
-#f2.write(unicode(json.dumps(webdata,ensure_ascii=False)))
-#print webdata
+
+
+
+
+f2=open("jsonfiles",'w')
+#we can receive domain google.com and, daum.net
+for x in range(2):
+	data=f.readline()
+	webdata=pythonwhois.get_whois(data)
+	data2=json.dumps(webdata,default=date_handler)
+	f2.write(data2)
+	#f2.write("\n ---------------------------------------------------- \n")
+
 f.close()
-#print(domain1)
-#status contacts nameservers
 f2.close()
+
